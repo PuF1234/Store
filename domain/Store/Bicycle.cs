79f25f82ce33ec6d7ef7d7a8580f94185c43559b
PuF1234 <rostik20041810@gmail.com>
2024-01-16 16:42:26 +0100

    Describe entity Bicycle

---
+++ domain/Store/Bicycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Store
+{
+    public class Bicycle
+    {
+        public int ID {  get; }
+        public string Title { get; }
+        public Bicycle(int id, string title)
+        {
+            ID = id;
+            Title = title;
+
+        }
+    }
+}
