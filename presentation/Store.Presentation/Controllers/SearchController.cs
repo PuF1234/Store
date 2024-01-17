6802c538028c2a3b958f586e478bbea2ee06090a
PuF1234 <rostik20041810@gmail.com>
2024-01-17 11:57:16 +0100

    Add page for search

---
+++ presentation/Store.Presentation/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Presentation.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBicycleRepos bicycleRepos;
+        public SearchController(IBicycleRepos bicycleRepos)
+        {
+            this.bicycleRepos = bicycleRepos;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var bicycles = bicycleRepos.GetAllByTitle(query);
+
+            return View(bicycles);
+        }
+    }
+}
