6e9d7c5412f62727ce3a782f29eca6dd62bae5c2
PuF1234 <rostik20041810@gmail.com>
2024-02-07 05:35:25 +0100

    Add fulltext search

---
+++ infrastructure/Store.Data.EF/Migrations/20240207043021_FullTextSearch.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Store.Data.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class FullTextSearch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE FULLTEXT CATALOG StoreFullTextCatalog AS DEFAULT", suppressTransaction: true);
+            migrationBuilder.Sql("CREATE FULLTEXT INDEX ON Bicycles(Producer, Title) KEY INDEX PK_Bicycles WITH STOPLIST = SYSTEM", suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FULLTEXT INDEX ON Bicycles", suppressTransaction: true);
+            migrationBuilder.Sql("DROP FULLTEXT CATALOG StoreFullTextCatalog", suppressTransaction: true);
+        }
+    }
+}
