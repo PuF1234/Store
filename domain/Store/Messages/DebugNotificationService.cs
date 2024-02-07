51ed240871ac1e88de1185d4a396e12557d79e78
PuF1234 <rostik20041810@gmail.com>
2024-02-07 17:41:01 +0100

    Send messages about end of order

--- domain/Store/Messages/DebugNotificationService.cs
+++ domain/Store/Messages/DebugNotificationService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Net.Mail;
+using System.Text;
 
 namespace Store.Messages
 {
@@ -7,6 +9,24 @@ namespace Store.Messages
         public void SendConfirmationCode(string cellPhone, int code)
         {
             Debug.WriteLine("Cellphone: {0}, code: {1:0000}.", cellPhone, code);
+        }
+
+        public void StartProcess(Order order)
+        {
+            using (var client = new SmtpClient())
+            {
+                var message = new MailMessage("from@at.my.domain", "to@at.my.domain");
+                message.Subject = "Order #" + order.Id;
+
+                var builder = new StringBuilder();
+                var s = "";
+                foreach(var item in order.Items) 
+                { 
+                    builder.Append("{0}, {1}", item.BicycleId, item.Count);
+                    s += string.Format("0{}");
+                    builder.AppendLine();
+                }
+            }
         }
     }
 }
